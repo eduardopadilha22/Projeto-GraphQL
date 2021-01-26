5bb64eee6b6601935003abbfd37a56c3c12789d6
Eduardo <epadilhatec@gmail.com>
2021-01-25 21:58:02 -0400

    conclusão.

--- frontend/src/componentes/usuario/Alterar.vue
+++ frontend/src/componentes/usuario/Alterar.vue
@@ -82,7 +82,50 @@ export default {
   },
   methods: {
     alterarUsuario() {
-      // Implementar
+      this.$api.mutate({
+        mutation: gql `
+
+          mutation(
+            $idFiltro:  Int
+            $emailFiltro: String
+            $nome: String
+            $email: String
+            $senha: String
+            $perfis: [PerfilFiltro]
+          ){
+            alterarUsuario(
+              filtro:{
+                id: $idFiltro
+                email: $emailFiltro
+              }
+              dados:{
+                nome: $nome
+                email: $email
+                senha: $senha
+                perfis: $perfis
+              }
+            ){
+              id nome email perfis { rotulo }
+            }
+          }
+        
+        `,
+        variables: {
+          idFiltro: this.filtro.id,
+          emailFiltro: this.filtro.email,
+          nome: this.usuario.nome,
+          email: this.usuario.email,
+          senha: this.usuario.senha,
+          perfis: this.perfisSelecionados
+        }
+      }).then(resultado => {
+        this.dados = resultado.data.alterarUsuario
+        this.erros = null
+        this.filtro = {}
+        this.usuario = {} 
+      }).catch(e => {
+        this.erros = e
+      })
     },
     obterPerfis() {
       this.$api
